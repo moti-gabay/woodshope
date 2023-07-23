3a94739459b83f9dbabf1526e894d49e58f147a0
Moti Gabay <motigabay18@gmail.com>
2023-07-23 16:49:30 +0300

    ok

--- paypal/index.js
+++ paypal/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
 //   });
 
 
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
 const environment = process.env.ENVIRONMENT || 'sandbox';
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
