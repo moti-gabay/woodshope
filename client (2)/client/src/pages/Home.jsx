2d00ed9fa67bcb5fa3c6285c9f92e3498fddd266
Motti770 <140093356+moti700@users.noreply.github.com>
2023-07-24 11:03:16 +0000

    ok

--- client (2)/client/src/pages/Home.jsx
+++ client (2)/client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     <div className="min-h-screen">
       <HomePage/>
       <div className="text-center font-semibold mt-[5%] mb-[2%]">
-        <h2 className='text-3xl sm:text-5xl lg:text-6xl mb-1 sm:mb-2'>Build By Your Own</h2>
+        <h2 className='text-3xl sm:text-5xl lg:text-6xl mb-1 sm:mb-2'>Build By Your wood</h2>
         <h3 className='text-xl sm:text-3xl lg:text-4xl'>Choose Wood Type</h3>   
         <h3><SlArrowDown className='mx-auto mt-5 text-2xl sm:text-4xl lg:text-5xl'/> </h3>
       </div>
